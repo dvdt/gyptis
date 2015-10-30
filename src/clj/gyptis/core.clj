26c7d5ac785c979ea9a8b6fd999278081c3a1444
David Tsao <davidtsao@gmail.com>
2015-10-30 18:32:57 -0500

    plots

--- src/clj/gyptis/core.clj
+++ src/clj/gyptis/core.clj
@@ -7,6 +7,19 @@
 
 (def ^:dynamic *current-plot-key* :default)
 
+(defn wrap-dims
+  [spec]
+  (util/merge-spec vega/top-level spec))
+
+(defn facet-global
+  "Facets the data by the *facet-x* fand *facet-y* keys. Scales are
+  the same for each subplot."
+  [{:keys [scales axes marks legends] [{data :values} & more] :data :as vega-spec}]
+  (let [facet-keyed-data (vega/ensure-facet-keys data)]
+    (prn facet-keyed-data)
+    (wrap-dims (vega/facet (update-in vega-spec [:marks 0 :from :data] (constantly nil))
+                           facet-keyed-data))))
+
 (defn plot
   "Plots the given vega-spec on the browser client connected by
   plot-key. If no client exists, a new one is created."
@@ -19,13 +32,10 @@
      (when-not (get-in @ws/connected-uids [:any plot-key])
        (view/new! :view-name plot-key)
        (alter-var-root #'*current-plot-key* (constantly plot-key)))
-     (view/plot! plot-key vega-spec)))
+     (view/plot! plot-key
+                 (-> vega-spec facet-global))))
   ([vega-spec]
    (plot vega-spec *current-plot-key*)))
-
-(defn wrap-top-data
-  [spec]
-  (util/merge-spec vega/top-level spec))
 
 (defn point
   [data]
@@ -33,18 +43,17 @@
         point-spec (vega/point data)
         facetted-spec (vega/facet (update-in point-spec [:marks 0 :from] (constantly nil))
                                   data)]
-    (wrap-top-data facetted-spec)))
+    (wrap-dims facetted-spec)))
 
 (defn stacked-bar
   [data]
   (let [data (vega/ensure-facet-keys data)
         spec (-> data
                  vega/stacked-bar
-                 (update-in [:data :transform 0 :groupby]
+                 #_(update-in [:data 1 :transform :groupby]
                             #(concat % [vega/*facet-x* vega/*facet-y*]))
-                 (update-in [:marks 0 :from] (constantly nil))
                  (vega/facet data))]
-    (wrap-top-data spec)))
+    (wrap-dims spec)))
 
 (defn bar
   [data]
@@ -55,9 +64,8 @@
   (let [data (vega/ensure-facet-keys data)
         spec (-> data
                  vega/dodged-bar
-                 #_(update-in [:marks 0 :from] (constantly nil))
                  (vega/facet data))]
-    (wrap-top-data spec)))
+    (wrap-dims spec)))
 
 (defn line
   [data]
