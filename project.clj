dd0a2ec9de40a92ac1ecc3d42fc59825251cba26
David Tsao <davidtsao@gmail.com>
2015-11-04 18:27:47 -0600

    Oops, latest stable cljs is indeed 145

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject gyptis "0.1.0-SNAPSHOT"
+(defproject gyptis "0.1.0"
   :description "A library for generating and viewing vega.js plots"
   :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.166" :scope "provided"]
+                 [org.clojure/clojurescript "1.7.145" :scope "provided"]
                  [org.clojure/core.async "0.2.371"]
                  [reagent "0.5.1"]
                  [ring "1.4.0"]
