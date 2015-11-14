2951b5d83295798b5e4faca5de814fdc7e9137e7
David Tsao <davidtsao@gmail.com>
2015-11-13 18:35:22 -0600

    bump gyptis-usage dependency

--- examples/gyptis-usage/project.clj
+++ examples/gyptis-usage/project.clj
@@ -7,4 +7,4 @@
                  [org.clojure/java.jdbc "0.4.2"]
                  [com.h2database/h2 "1.4.190"]
                  [clj-http "2.0.0"]
-                 [gyptis "0.1.0-SNAPSHOT"]])
+                 [gyptis "0.2.0"]])
